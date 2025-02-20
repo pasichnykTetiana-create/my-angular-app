669a8d77bf4892580f3a771941a187cf44b75d9b
pasichnykTetiana-create <pasichnyktetiana.de@gmail.com>
2025-02-20 09:27:20 +0100

    feat: Anzeige der vom Benutzer eingegebenen Daten auf der Seite hinzugefügt

--- src/components/material-card/material-card.component.ts
+++ src/components/material-card/material-card.component.ts
@@ -1,24 +1,77 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FloatLabelType, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core'; 
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-material-card',
-  standalone: true, 
   imports: [
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule,
+    MatSelectModule,
+    MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatNativeDateModule, 
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatDividerModule
   ],
   templateUrl: './material-card.component.html',
   styleUrls: ['./material-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MaterialCardComponent {}
+export class MaterialCardComponent {
+
+  readonly hideRequiredControl = new FormControl(true);
+  readonly floatLabelControl = new FormControl('always' as FloatLabelType);
+  readonly options = inject(FormBuilder).group({
+    hideRequired: this.hideRequiredControl,
+    floatLabel: this.floatLabelControl,
+  });
+
+  protected readonly hideRequired = toSignal(this.hideRequiredControl.valueChanges);
+  protected readonly floatLabel = toSignal(
+    this.floatLabelControl.valueChanges.pipe(map(v => v || 'always')),
+    {initialValue: 'always'}
+  );
+
+
+  profileForm = new FormGroup({
+    vorName: new FormControl(''),
+    nachName: new FormControl(''),
+    stadt: new FormControl(''),
+    zeit: new FormControl(''),
+  });
+
+
+  submitInput: any = {}
+
+
+  onSubmit() {
+    const formData = this.profileForm.value;
+
+
+    if (formData.zeit) {
+      const dateObj = new Date(formData.zeit);
+      formData.zeit = dateObj.toLocaleDateString('de-Uk');
+    }
+
+    this.submitInput = formData;
+    console.log(this.submitInput);
+  }
+
+}
